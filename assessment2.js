687367c1b71ffb076195867f447085a699e8da44
Winfida-Obinyo <obinyowinfridaa@gmail.com>
2023-05-01 09:01:28 +0300

    js assessment

---
+++ assessment2.js
@@ -0,0 +1,81 @@
+// const books = [
+// { title: 'The Catcher in the Rye', author: 'J.D. Salinger', publicationYear: 1951,
+// isAvailable: true },
+// { title: 'Great Expectations', author: 'Charles Dickens', publicationYear: 1861,
+// isAvailable: false },
+// { title: 'Crime and Punishment', author: 'Fyodor Dostoevsky', publicationYear:
+// 1866, isAvailable: true },
+// { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', publicationYear: 1925,
+// isAvailable: false },
+// { title: 'The Odyssey', author: 'Homer', publicationYear: -800, isAvailable: true },
+// ];
+// Create a function called getAvailableBooks that returns an array of all available
+// books.
+function getAvailableBooks(bookks){
+    return getAvailableBooks.filter(function(nameBookks){
+        return nameBookks
+
+    })
+
+}
+// Create a function getBooksByAuthor that takes an author's name as an argument and
+// returns an array of all books by that author.
+function getBooksByAuthor(name){
+    return getBooksByAuthor.filter(function(authorname){
+        return authorname === author(name)
+    }).map(name)
+
+}
+const books = [
+    { title: 'The Catcher in the Rye', author: 'J.D. Salinger', publicationYear: 1951,
+    isAvailable: true },
+    { title: 'Great Expectations', author: 'Charles Dickens', publicationYear: 1861,
+    isAvailable: false },
+    { title: 'Crime and Punishment', author: 'Fyodor Dostoevsky', publicationYear:
+    1866, isAvailable: true },
+    { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', publicationYear: 1925,
+    isAvailable: false },
+    { title: 'The Odyssey', author: 'Homer', publicationYear: -800, isAvailable: true },
+    ];
+const getAuthor=(author)
+console.log(getBooksByAuthor)
+// Create a function addNewBook that takes a book object as an argument and adds it
+// to the library, ensuring that the new book has all required properties (title, author,
+// publicationYear, and isAvailable).
+function addNewBook(object){
+    return addNewBook.reduce =>();
+
+}
+
+// Create a function checkoutBook that takes a book title as an argument and changes
+// the book's isAvailable property to false. If the book is not found in the library, the
+// function should return a message indicating that the book is not available.
+function checkoutBook(bookTitle){
+    return checkoutBook.filter(function(title){
+        if (the book === title){
+            return ("book" is true)
+        }
+        return bookTitle === title;
+    
+
+
+
+
+}
+// Create a function returnBook that takes a book title as an argument and changes the
+// book's isAvailable property to true. If the book is not found in the library, the function
+// should return a message indicating that the book does not belong to the library.
+function returnBook(bookTitle){ 
+
+    for (title in books){
+        if (title === bookTitle){
+            return title
+ }
+ else {
+    
+ }
+
+        
+  } 
+   }
+   
